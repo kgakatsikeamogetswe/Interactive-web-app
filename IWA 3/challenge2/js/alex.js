62529dfdeb79d51d7d4b98f4d7bf865a83e8bea7
kgakatsikeamogetswe <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2023-03-29 11:13:45 +0200

    IWA 3

---
+++ IWA 3/challenge2/js/alex.js
@@ -0,0 +1,8 @@
+// johannes.js
+
+const firstname = "Alex"
+const surname = "Mokgoatlheng"
+const role = "Head of Marketing"
+
+const display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#alex').innerText = display
